1666b1163c74e44435998aa06d28e11025d0a850
dangvanduc90 <dangvanduc0@gmail.com>
2020-02-01 11:31:51 +0700

    first commit

---
+++ application/config/email.php
@@ -0,0 +1,28 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Email
+| -------------------------------------------------------------------------
+| This file lets you define parameters for sending emails.
+| Please see the user guide for info:
+|
+|	http://codeigniter.com/user_guide/libraries/email.html
+|
+*/
+$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_user'] = 'huanhd@envi-tech.com.vn';
+$config['smtp_pass'] = 'Vniti$9Hdvn@123';
+$config['smtp_port'] = '587';
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['protocol'] = 'smtp';
+$config['smtp_crypto'] = 'tls';
+
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['newline'] = "\r\n";
+
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */
